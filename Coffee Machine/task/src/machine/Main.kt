a915ab3e1ab006f9105e720723113a90ecb61acc
Ahmed Mohamed Abdelaal <aabdelaal@gmail.com>
2022-03-03 21:01:55 +0200

    final stage (6)

--- Coffee Machine/task/src/machine/Main.kt
+++ Coffee Machine/task/src/machine/Main.kt
@@ -1,76 +1,97 @@
 package machine
 
-fun main() {
-//    println("Starting to make a coffee")
-//    println("Grinding coffee beans")
-//    println("Boiling water")
-//    println("Mixing boiled water with crushed coffee beans")
-//    println("Pouring coffee into the cup")
-//    println("Pouring some milk into the cup")
-//    println("Coffee is ready!")
-//    println("Write how many cups of coffee you will need:")
-    // stage 2
-//    val cups = readln().toInt()
-//    println("For $cups of coffee you will need:")
-//    println("${cups * 200} ml of water")
-//    println("${cups * 50} ml of milk")
-//    println("${cups * 15} g of coffee beans")
-//stage 3
-//    print("water ? ")
-//    val water = readln().toInt()
-//    print("milk ? ")
-//    val milk = readln().toInt()
-//    print("beans ? ")
-//    val beans = readln().toInt()
-//    print("cups ? ")
-//    val cups = readln().toInt()
-//
-//    val waterCups = water / 200
-//    val milkCups = milk / 50
-//    val beansCups = beans / 15
-//
-//    val minCups = listOf(waterCups, milkCups, beansCups).minOrNull()!!
-//    if (minCups == cups) {
-//        println("Yes, I can make that amount of coffee")
-//    } else if (minCups > cups) {
-//        println("Yes, I can make that amount of coffee (and even ${minCups - cups} more than that)")
-//    } else {
-//        println("No, I can make only $minCups cups of coffee")
-//    }
-//stage 4
-    var water = 400
-    var milk = 540
-    var beans = 120
-    var d_cups = 9
-    var money = 550
+enum class MachineState {
+    STARTED, TAKE_ACTION, CLOSED
+}
 
-    var _water = 250
-    var _milk = 0
-    var _beans = 16
-    var _cost = 4
-    val espresso = listOf(_water, _milk, _beans, _cost)
+class CoffieMachine {
+    private var water = 400
+    private var milk = 540
+    private var beans = 120
+    private var d_cups = 9
+    private var money = 550
 
-    _water = 350
-    _milk = 75
-    _beans = 20
-    _cost = 7
-    val latte = listOf(_water, _milk, _beans, _cost)
+    // water, milk,beans,cost
+    private val espresso = listOf(250, 0, 16, 4)
+    private val latte = listOf(350, 75, 20, 7)
+    private val cappuccino = listOf(200, 100, 12, 6)
 
-    _water = 200
-    _milk = 100
-    _beans = 12
-    _cost = 6
+    private var state = MachineState.STARTED
 
-    val cappuccino = listOf(_water, _milk, _beans, _cost)
+    init {
+        dispatch()
+    }
 
-    fun buy() {
+    fun getInput(input: String): MachineState {
+        return dispatch(input)
+    }
+
+    private fun dispatch(input: String = ""): MachineState {
+        when (state) {
+            MachineState.STARTED -> {
+                displayCommandList()
+
+            }
+            MachineState.TAKE_ACTION -> {
+                when (input) {
+                    "buy" -> {
+                        buy()
+                        state = MachineState.STARTED
+                        displayCommandList()
+                    }
+                    "fill" -> {
+                        fill()
+                        state = MachineState.STARTED
+                        displayCommandList()
+                    }
+                    "take" -> {
+                        take()
+                        state = MachineState.STARTED
+                        displayCommandList()
+                    }
+                    "remaining" -> {
+                        remaining()
+                        state = MachineState.STARTED
+                        displayCommandList()
+                    }
+                    "exit" -> exit()
+                }
+
+            }
+//            MachineState.REMAINING -> remaining()
+            else -> {}
+        }
+        return state
+    }
+
+    private fun exit() {
+        state = MachineState.CLOSED
+    }
+
+    private fun displayCommandList() {
+        print("Write action (buy, fill, take, remaining, exit): >")
+        state = MachineState.TAKE_ACTION
+    }
+
+    private fun remaining() {
+        println(
+            "\nThe coffee machine has:\n" +
+                    "$water ml of water\n" +
+                    "$milk ml of milk\n" +
+                    "$beans g of coffee beans\n" +
+                    "$d_cups disposable cups\n" +
+                    "\$$money of money\n"
+        )
+    }
+
+    private fun buy() {
         println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu: >")
         val buyWhat = readln()
         val what: List<Int>
-        when (buyWhat) {
-            "1" -> what = espresso
-            "2" -> what = latte
-            "3" -> what = cappuccino
+        what = when (buyWhat) {
+            "1" -> espresso
+            "2" -> latte
+            "3" -> cappuccino
             "back" -> return
 
             else -> return
@@ -101,7 +122,7 @@ fun main() {
 
     }
 
-    fun fill() {
+    private fun fill() {
         print("water ? ")
         val w = readln().toInt()
         print("milk ? ")
@@ -120,32 +141,25 @@ fun main() {
 
     }
 
-    fun remaining() {
-        println(
-            "The coffee machine has:\n" +
-                    "$water ml of water\n" +
-                    "$milk ml of milk\n" +
-                    "$beans g of coffee beans\n" +
-                    "$d_cups disposable cups\n" +
-                    "\$$money of money"
-        )
-    }
 
-    fun take() {
+    private fun take() {
         println("I gave you \$$money")
         money = 0
     }
 
+    fun isWorking(): Boolean = state != MachineState.CLOSED
 
-    var command = ""
-    while (command != "exit") {
-        println("Write action (buy, fill, take, remaining, exit): >")
+
+}
+
+fun main() {
+
+    val machine = CoffieMachine()
+
+    var command: String
+    while (machine.isWorking()) {
+
         command = readln()
-        when (command) {
-            "buy" -> buy()
-            "fill" -> fill()
-            "take" -> take()
-            "remaining" -> remaining()
-        }
+        machine.getInput(command)
     }
 }
